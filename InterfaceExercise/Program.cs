7f3c696a01f0dd5f418baba0bf536aea8b2e2dda
SupremeTurnerTrueCoders <email@example.com>
2024-06-25 20:46:53 -0400

    Completed Interface Exercise

--- InterfaceExercise/Program.cs
+++ InterfaceExercise/Program.cs
@@ -8,15 +8,35 @@ namespace InterfaceExercise
     {
         static void Main(string[] args)
         {
-           var car = new Car();
-           var truck = new Truck();
+            var car = new Car()
+            {
+                NumberOfWheels = 4,
+                EngineType = 4,
+                ExteriorColor =  "Yellow",
+                InteriorColor = "Blue",
+                
+            };
+            var truck = new Truck()
+            {
+                NumberOfWheels = 8,
+                EngineType = 4,
+                ExteriorColor = "Black",
+                InteriorColor = "Blue",
+                
+
+            };
            var suv = new SUV();
+            suv.NumberOfWheels = 4;
+            suv.EngineType = 5;
+            suv.ExteriorColor = "Red";
+            suv.InteriorColor = "Black";
+
 
             var vehicles = new List<IVehicle>() { car, truck, suv };
 
             foreach (var vehicle in vehicles)
             {
-                
+                Console.WriteLine($" {vehicle.NumberOfWheels} {vehicle.EngineType} {vehicle.ExteriorColor} {vehicle.InteriorColor}");
                 vehicle.ChangeGears(true);
                 
             } 
